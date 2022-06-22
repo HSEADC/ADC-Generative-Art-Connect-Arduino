3258d2ab970f6d197974f222cffe01596eabbd6d
ZakharDay <zakhar.day@gmail.com>
2022-06-22 14:56:00 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './index.scss'
 let radius = 100
 
 // From https://habr.com/ru/post/516334/
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,22 @@
-const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const webpack = require('webpack')
+const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './index.js'
-  },
   devtool: 'inline-source-map',
   devServer: {
     static: './dev_build'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development'
-    })
-  ],
+  entry: {
+    index: './src/index.js'
+  },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dev_build'),
     clean: true,
     publicPath: '/'
   },
@@ -25,15 +24,53 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
+            cacheDirectory: true
           }
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(ttf|otf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
         },
         exclude: /node_modules/
       }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()]
   }
 }
